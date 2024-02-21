2b19956406b428aa2a3798ddc62146b9b751ff24
gezinnn <geovannialmeida01@gmail.com>
2024-02-21 12:55:57 -0300

    Adicionar arquivos de projeto.

---
+++ DAO/UsuarioDAO.cs
@@ -0,0 +1,32 @@
+﻿using APIUsuarios.DTO;
+using MySql.Data.MySqlClient;
+
+namespace APIUsuarios.DAO
+{
+    public class UsuarioDAO
+    {
+            public List<UsuarioDTO> ListarUsuarios()
+            {
+                var usuarios = new List<UsuarioDTO>();
+                var conexao = ConnectionFactory.Build();
+                conexao.Open();
+                var query = "SELECT*FROM Usuarios";
+                var comando = new MySqlCommand(query, conexao);
+                var dataReader = comando.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                var usuario = new UsuarioDTO();
+                usuario.ID_Usu = int.Parse(dataReader["ID_Usu"].ToString());
+                usuario.Nome = dataReader["Nome"].ToString();
+                usuario.Email = dataReader["Email"].ToString();
+                usuario.Telefone = dataReader["Telefone"].ToString();
+
+                usuarios.Add(usuario);
+
+            }
+                conexao.Close();
+                return usuarios;
+            }        
+    }
+}
---
+++ DTO/UsuarioDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APIUsuarios.DTO
+{
+    public class UsuarioDTO
+    {
+        public int ID_Usu{ get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set;}
+        public string Telefone { get; set;}
+    }
+}

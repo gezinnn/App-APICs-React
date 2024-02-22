0f2d678fa90ee9de0f6403df683610b08b4e9187
gezinnn <geovannialmeida01@gmail.com>
2024-02-22 16:14:38 -0300

    Atualizando com o CadastrarUsuario() e VerficarUsuario()

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
 ﻿using APIUsuarios.DAO;
+using APIUsuarios.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,22 @@ namespace APIUsuarios.Controllers
             var dao = new UsuarioDAO();
             var usuarios = dao.ListarUsuarios();
             return Ok(usuarios);
+        }
+        [HttpPost]
+        [Route("cadastrar")]
+        public IActionResult Cadastrar([FromBody] UsuarioDTO usuario)
+        {
+            var dao = new UsuarioDAO();
+
+            var usuarioExiste = dao.VerificarUsuario(usuario);
+
+            if (usuarioExiste)
+            {
+                var mensagem = "E-mail já existe na base de dados";
+                return Conflict(mensagem);
+            }
+            dao.Cadastrar(usuario);
+            return Ok();
         }
     }
 }
--- DAO/UsuarioDAO.cs
+++ DAO/UsuarioDAO.cs
@@ -1,32 +1,70 @@
-﻿using APIUsuarios.DTO;
+﻿using System;
+using System.Collections.Generic;
+using APIUsuarios.DTO;
 using MySql.Data.MySqlClient;
 
 namespace APIUsuarios.DAO
 {
     public class UsuarioDAO
     {
-            public List<UsuarioDTO> ListarUsuarios()
+        public List<UsuarioDTO> ListarUsuarios()
+        {
+            var usuarios = new List<UsuarioDTO>();
+            using (var conexao = ConnectionFactory.Build())
             {
-                var usuarios = new List<UsuarioDTO>();
-                var conexao = ConnectionFactory.Build();
                 conexao.Open();
-                var query = "SELECT*FROM Usuarios";
-                var comando = new MySqlCommand(query, conexao);
-                var dataReader = comando.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                var usuario = new UsuarioDTO();
-                usuario.ID_Usu = int.Parse(dataReader["ID_Usu"].ToString());
-                usuario.Nome = dataReader["Nome"].ToString();
-                usuario.Email = dataReader["Email"].ToString();
-                usuario.Telefone = dataReader["Telefone"].ToString();
-
-                usuarios.Add(usuario);
-
+                var query = "SELECT * FROM Usuarios";
+                using (var comando = new MySqlCommand(query, conexao))
+                {
+                    using (var dataReader = comando.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            var usuario = new UsuarioDTO();
+                            usuario.ID_Usu = Convert.ToInt32(dataReader["ID_Usu"]);
+                            usuario.Nome = dataReader["Nome"].ToString();
+                            usuario.Email = dataReader["Email"].ToString();
+                            usuario.Telefone = dataReader["Telefone"].ToString();
+                            usuarios.Add(usuario);
+                        }
+                    }
+                }
             }
-                conexao.Close();
-                return usuarios;
-            }        
+            return usuarios;
+        }
+
+        public void Cadastrar(UsuarioDTO usuario)
+        {
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
+                var query = @"INSERT INTO Usuarios (Nome, Email, Telefone) VALUES 
+                            (@nome, @email, @telefone)";
+                using (var comando = new MySqlCommand(query, conexao))
+                {
+                    comando.Parameters.AddWithValue("@nome", usuario.Nome);
+                    comando.Parameters.AddWithValue("@email", usuario.Email);
+                    comando.Parameters.AddWithValue("@telefone", usuario.Telefone);
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public bool VerificarUsuario(UsuarioDTO usuario)
+        {
+            bool usuarioExiste = false;
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
+                var query = "SELECT COUNT(*) FROM Usuarios WHERE Email = @email";
+                using (var comando = new MySqlCommand(query, conexao))
+                {
+                    comando.Parameters.AddWithValue("@email", usuario.Email);
+                    int count = Convert.ToInt32(comando.ExecuteScalar());
+                    usuarioExiste = count > 0;
+                }
+            }
+            return usuarioExiste;
+        }
     }
 }
